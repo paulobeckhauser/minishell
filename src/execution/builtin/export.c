7f8a649a7467e54d1e90d3116efba777bc813869
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2r14s7.42wolfsburg.de>
2024-03-27 12:07:19 +0100

    norminette correction for export files

--- src/execution/builtin/export.c
+++ src/execution/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 18:50:07 by pabeckha          #+#    #+#             */
-/*   Updated: 2024/03/27 11:59:24 by pabeckha         ###   ########.fr       */
+/*   Updated: 2024/03/27 12:06:06 by pabeckha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,8 @@ int	is_export_command(char **command)
 
 static void	export_without_args(t_info *structure)
 {
-	int	i;
-	char **array;
-
-	// selectiton_sort_variables(envp);
-	// i = 0;
-	// while (envp[i])
-	// {
-	// 	array = ft_split(envp[i], '=');
-	// 	if (array[1])
-	// 		printf("declare -x %s=\"%s\"\n", array[0], array[1]);
-	// 	else
-	// 		printf("declare -x %s\n", envp[i]);
-	// 	free_2d_array(array);
-	// 	i++;
-	// }
+	int		i;
+	char	**array;
 
 	selectiton_sort_variables(structure->envp);
 	i = 0;
@@ -49,73 +36,29 @@ static void	export_without_args(t_info *structure)
 	}
 }
 
-// check if env variable already exists
-// static int check_env_variable(char **envp, char **command)
-// {
-// 	char *temp;
-// 	int check_exist;
-	
-// 	int i;
-// 	int j;
-// 	int len;
-// 	char *str_temp;
-
-// 	check_exist = 0;
-// 	i = 0;
-// 	while(envp[i])
-// 	{
-// 		len = 0;
-// 		while(envp[i][len] && envp[i][len] != '=')
-// 			len++;
-// 		str_temp = (char *)malloc((len + 1) * sizeof(char));
-// 		j = 0;
-// 		while(envp[i][j] && envp[i][j] != '=')
-// 		{
-// 			str_temp[j] = envp[i][j];
-// 			j++;
-// 		}
-// 		str_temp[j] = '\0';
-// 		if (ft_strcmp(str_temp, command[1]) == 0)
-// 		{
-// 			check_exist = 1;
-// 			break;
-// 		}
-// 		free(str_temp);
-// 		i++;
-// 	}	
-// 	return (check_exist);
-// }
-
 static void	export_with_args(t_info *structure)
 {
-	int	i;
-	int j;
-	int check;
-	char **array;
+	int		i;
+	int		j;
+	int		check;
 
 	check = 0;
 	j = 0;
-	while(structure->table->arr[1][j])
+	while (structure->table->arr[1][j])
 	{
 		if (structure->table->arr[1][j] == '=')
 		{
 			check++;
-			break;
+			break ;
 		}
-		j++;		
+		j++;
 	}
-
 	if (check == 1)
-	{
-		array = ft_split(structure->table->arr[1], '=');
-		delete_string(structure, array[0]);
-		add_to_envp(structure, structure->table->arr[1]);
-		free_2d_array(array);
-	}
+		replace_value_envp(structure);
 	else
 	{
 		if (check_env_variable(structure))
-			return;
+			return ;
 		else
 			add_to_envp(structure, structure->table->arr[1]);
 	}
