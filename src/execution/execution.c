516ab18b45795ce61dd0a8462482c80384087cfb
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2r13s8.42wolfsburg.de>
2024-03-29 13:19:21 +0100

    Correction number of commands --initialize variable

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -6,7 +6,7 @@
 /*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 16:43:37 by pabeckha          #+#    #+#             */
-/*   Updated: 2024/03/29 09:20:10 by pabeckha         ###   ########.fr       */
+/*   Updated: 2024/03/29 13:18:37 by pabeckha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 void	execution(t_info *structure, t_prompt *prompt)
 {
-	pid_t	child_pid;
-
 	get_number_commands(structure);
 	store_commands(structure);
 	store_path_commands(structure);
--- src/execution/get_number_commands.c
+++ src/execution/get_number_commands.c
@@ -6,7 +6,7 @@
 /*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 12:49:06 by pabeckha          #+#    #+#             */
-/*   Updated: 2024/03/29 09:21:06 by pabeckha         ###   ########.fr       */
+/*   Updated: 2024/03/29 13:17:14 by pabeckha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	get_number_commands(t_info *structure)
 	t_cmd	*current_table;
 	int		number_commands;
 
+	number_commands = 0;
 	current_table = structure->table;
 	while (current_table)
 	{
