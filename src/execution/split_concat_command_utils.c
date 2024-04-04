a42cdfd1e7b663a24928bee7f53f2bc525fd86ee
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2r14s7.42wolfsburg.de>
2024-04-04 14:51:02 +0200

    Norminette correction for execution files

--- src/execution/split_concat_command_utils.c
+++ src/execution/split_concat_command_utils.c
@@ -6,11 +6,11 @@
 /*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 11:49:45 by pabeckha          #+#    #+#             */
-/*   Updated: 2024/03/11 15:26:20 by pabeckha         ###   ########.fr       */
+/*   Updated: 2024/04/04 14:49:13 by pabeckha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../../inc/minishell.h"
+#include "../../inc/minishell.h"
 
 void	*ft_free(char **strs, int count)
 {
@@ -35,22 +35,3 @@ char	**check_and_free(char **array, int j)
 	}
 	return (array);
 }
-
-// void	copy_string(t_info *structure, int first_command_position)
-// {
-// 	int	j;
-// 	int	len_string;
-
-// 	j = 0;
-// 	while (first_command_position <= structure->argc - 2)
-// 	{
-// 		len_string = ft_strlen(structure->argv[first_command_position]);
-// 		structure->full_string[j] = (char *)malloc((len_string + 1)
-// 				* sizeof(char));
-// 		ft_strlcpy(structure->full_string[j],
-// 			structure->argv[first_command_position], len_string + 1);
-// 		first_command_position++;
-// 		j++;
-// 	}
-// 	structure->full_string[j] = NULL;
-// }
