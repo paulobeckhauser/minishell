b732eeef027b70be3a13e31547ab124253c1bc17
Paulo Ricardo Beckhauser De Araujo <pabeckha@c1r2s6.42wolfsburg.de>
2024-04-17 21:02:59 +0200

    Execution norminette corrections

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -6,41 +6,42 @@
 /*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 16:43:37 by pabeckha          #+#    #+#             */
-/*   Updated: 2024/04/17 13:45:08 by pabeckha         ###   ########.fr       */
+/*   Updated: 2024/04/17 21:02:28 by pabeckha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
+static void	pipeline_execution(t_info *structure)
+{
+	store_commands(structure);
+	store_path_commands(structure);
+	pipes_implementation(structure);
+}
+
 bool	execution(t_info *structure)
 {
-	int		test;
-	int		i;
+	int		flag_cur_folder;
 	char	cwd[PATH_MAX];
 
-	i = 0;
-	test = 0;
+	flag_cur_folder = 0;
 	handle_key_combos_execution();
 	get_number_commands(structure);
 	structure->folder = getcwd(cwd, sizeof(cwd));
-	if (structure->table->type == BUILTIN_CMD && structure->number_commands == 1)
+	if (structure->table->type == BUILTIN_CMD
+		&& structure->number_commands == 1)
 		builtin_execution(structure);
 	else
 	{
-		test = get_path_env(structure);
-		if (test == 0)
+		flag_cur_folder = get_path_env(structure);
+		if (flag_cur_folder == 0)
 		{
-			i = 0;
 			while (structure->table)
 				structure->table = structure->table->next;
 			structure->last_exit_status = EX_COMM_NOTFOUND;
 		}
 		else
-		{
-			store_commands(structure);
-			store_path_commands(structure);
-			pipes_implementation(structure);
-		}
+			pipeline_execution(structure);
 	}
 	return (true);
 }
